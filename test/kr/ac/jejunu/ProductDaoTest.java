cb7a17aec9221de6f8642d836b59609f2fe8e10c
Dongha Kim <ghost9087@kailua.co.kr>
2017-04-21 16:30:57 +0900

    상품 추가 기능 추가

--- test/kr/ac/jejunu/ProductDaoTest.java
+++ test/kr/ac/jejunu/ProductDaoTest.java
@@ -2,7 +2,9 @@ package kr.ac.jejunu;
 
 import org.junit.Test;
 
+import java.sql.Date;
 import java.sql.SQLException;
+import java.util.Random;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -19,5 +21,25 @@ public class ProductDaoTest {
         assertThat(id, is(product.getId()));
         assertThat(title, is(product.getTitle()));
         assertThat(price, is(product.getPrice()));
+    }
+    @Test
+    public void add() throws SQLException, ClassNotFoundException {
+        Product product = new Product();
+        Random random = new Random();
+        Long id = random.nextLong() % 2000L;
+        String title = "파스타 소스";
+        Integer price = 2000;
+
+        product.setId(id);
+        product.setTitle(title);
+        product.setPrice(price);
+
+        ProductDao productDao = new ProductDao();
+        productDao.add(product);
+
+        Product insertedProduct = productDao.get(id);
+        assertThat(insertedProduct.getTitle(), is(title));
+        assertThat(insertedProduct.getPrice(), is(price));
+        assertThat(insertedProduct.getId(), is(id));
     }
 }
