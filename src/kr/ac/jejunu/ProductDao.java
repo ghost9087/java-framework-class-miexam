ef4853604de671858c4f64ca0fa9b457c756de9b
Dongha Kim <ghost9087@gmail.com>
2017-04-21 21:40:08 +0900

    jdbcContext에서 jdbcTemplate로 전환

--- src/kr/ac/jejunu/ProductDao.java
+++ src/kr/ac/jejunu/ProductDao.java
@@ -1,40 +1,60 @@
 package kr.ac.jejunu;
 
-import java.sql.PreparedStatement;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ProductDao {
-    private JdbcContext jdbcContext;
+    private JdbcTemplate jdbcTemplate;
 
     public Product get(Long id) throws SQLException, ClassNotFoundException {
         String sql = "select * from product where id = ?";
         Object[] params = new Object[]{id};
-        return jdbcContext.queryForObject(sql, params);
+        Product product = null;
+        try {
+            product = jdbcTemplate.queryForObject(sql, params, new RowMapper<Product>() {
+                @Override
+                public Product mapRow(ResultSet resultSet, int i) throws SQLException {
+                    Product productFromQuery = new Product();
+                    productFromQuery.setId(resultSet.getLong("id"));
+                    productFromQuery.setTitle(resultSet.getString("title"));
+                    productFromQuery.setPrice(resultSet.getInt("price"));
+
+                    return productFromQuery;
+                }
+            });
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
+        return product;
     }
 
     public void update(Product product) throws SQLException, ClassNotFoundException {
         String sql = "update product set title=?, price=? where id =?";
         Object[] params = new Object[]{product.getTitle(), product.getPrice(), product.getId()};
 
-        jdbcContext.update(sql, params);
+        jdbcTemplate.update(sql, params);
     }
 
     public void add(Product product) throws SQLException, ClassNotFoundException {
         String sql = "insert into product(id, title, price) VALUES (?, ?, ?)";
         Object[] params = new Object[]{product.getId(), product.getTitle(), product.getPrice()};
 
-        jdbcContext.update(sql, params);
+        jdbcTemplate.update(sql, params);
     }
 
     public void delete(Long id) throws SQLException, ClassNotFoundException {
         String sql = "delete from product where id=?";
         Object[] params = new Object[]{id};
 
-        jdbcContext.update(sql, params);
+        jdbcTemplate.update(sql, params);
     }
 
 
-    public void setJdbcContext(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 }
