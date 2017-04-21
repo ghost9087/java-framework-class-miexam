bc7cabccd58639e0ba48c9be6c798b18270b8118
Dongha Kim <ghost9087@kailua.co.kr>
2017-04-21 17:35:53 +0900

    exception발생시 connection 처리

--- src/kr/ac/jejunu/ProductDao.java
+++ src/kr/ac/jejunu/ProductDao.java
@@ -10,31 +10,73 @@ public class ProductDao {
         this.dataSource = dataSource;
     }
 
-    public Product get(Long id) throws ClassNotFoundException, SQLException {
+    public Product get(Long id) throws SQLException {
         Connection connection = dataSource.getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement("select * from product where id = ?");
-        preparedStatement.setLong(1, id);
-        ResultSet resultSet = preparedStatement.executeQuery();
-        resultSet.next();
-        Product product = new Product();
-        product.setId(resultSet.getLong("id"));
-        product.setTitle(resultSet.getString("title"));
-        product.setPrice(resultSet.getInt("price"));
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
+        Product product = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            preparedStatement = connection.prepareStatement("select * from product where id = ?");
+            preparedStatement.setLong(1, id);
+            resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            product = new Product();
+            product.setId(resultSet.getLong("id"));
+            product.setTitle(resultSet.getString("title"));
+            product.setPrice(resultSet.getInt("price"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (resultSet != null)
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            if (preparedStatement != null)
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+            if (connection != null)
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+        }
         return product;
     }
 
-    public void add(Product product) throws ClassNotFoundException, SQLException {
+    public void add(Product product) throws SQLException {
         Connection connection = dataSource.getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement("insert into product(id, title, price) VALUES (?, ?, ?)");
-        preparedStatement.setLong(1, product.getId());
-        preparedStatement.setString(2, product.getTitle());
-        preparedStatement.setInt(3, product.getPrice());
-        preparedStatement.executeUpdate();
+        PreparedStatement preparedStatement = null;
 
-        preparedStatement.close();
-        connection.close();
+        try {
+            preparedStatement = connection.prepareStatement("insert into product(id, title, price) VALUES (?, ?, ?)");
+            preparedStatement.setLong(1, product.getId());
+            preparedStatement.setString(2, product.getTitle());
+            preparedStatement.setInt(3, product.getPrice());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (preparedStatement != null)
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            if(connection != null)
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+        }
     }
 }
